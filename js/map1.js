8be375d0f8493f27d653c8d9632976ed24014875
PhilipKleemann <philipjk@uw.edu>
2026-01-28 22:20:39 -0800

    s

--- js/map1.js
+++ js/map1.js
@@ -4,7 +4,7 @@
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/dark-v10', // style URL
-            zoom: 3, // starting zoom
+            zoom: 3.3, // starting zoom
             center: [-100, 40] // starting center
         });
 
